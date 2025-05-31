bf2ffb303baf8f78aa2955247d5aea17acb8a236
Danielle Nova <99331448+oNovalSliveDNo@users.noreply.github.com>
2025-06-01 02:49:15 +1000

    Add files via upload

---
+++ chapter11/new_graphics_window.py
@@ -0,0 +1,7 @@
+# Новое графическое окно
+# Демонстрирует создание графического окна
+
+from superwires import games
+
+games.init(screen_width=640, screen_height=480, fps=50)
+games.screen.mainloop()
